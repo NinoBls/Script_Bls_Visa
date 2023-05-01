56d15576700535a3c0a8583f5d26a1093594b6ea
NinoBls <132262744+NinoBls@users.noreply.github.com>
2023-05-01 23:10:12 +0100

    Add files via upload

---
+++ Bls_alert.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         BLS alert
+// @namespace    http://tampermonkey.net/
+// @version      2.0.3
+// @description  bls aler  P|R|C Clean Refresh Alarm Notif
+// @author       @Itsmaarouf
+// @match        *://algeria.blsspainvisa.com/*
+// @grant        none
+// ==/UserScript==
+
+var gun=setInterval(function(){
+    if (document.getElementsByClassName('day activeClass').length !==0) {
+        setTimeout(function(){window.location.reload(1);},80* 1000);;
+        }
+    if (document.getElementsByClassName('day activeClass').length !==0){
+      new Audio('https://www.soundjay.com/mechanical/sounds/smoke-detector-1.mp3').play();
+      clearInterval(gun);
+    }
+}, 1000)
